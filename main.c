99fa959ca7fe8551801763005e85dcfad376d4d6
Benjamín Pamies <benja90.cox@gmail.com>
2016-12-27 21:45:29 +0100

    Lectura y escritura con fread y fwrite.

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 # include <stdio.h>
 
+const int TAM = 8000;
+
+void
+escribirFichero( char caracteres[], int num ) {
+	FILE *fichero = fopen( "salida.txt", "w" );
+	fwrite( caracteres, sizeof(char), num, fichero );
+	fclose( fichero );
+}
+
 void
 leerFichero( char* nombre ) {
 	FILE *fichero = fopen( nombre, "r" );
 	if( fichero != NULL ) {
-		char caracter;
-		while( feof( fichero ) == 0 ) {
-			caracter = fgetc( fichero );
-			printf( "%c", caracter );
+		int num;
+		char caracteres[TAM+1];
+		while( !feof( fichero ) ) {
+			num = fread( caracteres, sizeof(char), TAM, fichero );
+			caracteres[num*sizeof(char)] = '\0';
 		}
+		escribirFichero( caracteres, num );
 		fclose( fichero );
 	} else {
 		printf( "Error al abrir el fichero.\n" );
